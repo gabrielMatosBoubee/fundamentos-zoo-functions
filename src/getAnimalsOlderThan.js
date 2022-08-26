3a02262a49504c98859e0623c208bbf21b910a66
petromamboubee <gm.boubee@gmail.com>
2022-08-26 00:18:07 -0300

    requisito 1 e 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const resultado = species.filter((element) => element.name === animal);
+  const [pedrinho] = resultado;
+  const { residents } = pedrinho;
+  const solucao = residents.every((element) => element.age >= age);
+  return solucao;
 }
-
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const bichos = species.filter(element => {
+    return ids.some(e => {
+      return element.id === e
+    })
+    })
+  return bichos
 }
-
+console.log(getSpeciesByIds(data))
 module.exports = getSpeciesByIds;
