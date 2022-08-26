4c40a0e9e19c93ce42c6e257e03d490257c3518b
petromamboubee <gm.boubee@gmail.com>
2022-08-26 00:40:33 -0300

    finalizei a 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const resultado = employees.find((element) =>
+    element.firstName === employeeName
+  || element.lastName === employeeName);
+  return resultado === undefined ? {} : resultado;
 }
 
 module.exports = getEmployeeByName;
