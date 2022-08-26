daa1832daecce45bc0b5449d45406002603ce6cf
petromamboubee <gm.boubee@gmail.com>
2022-08-26 17:16:45 -0300

    finalizei o requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const crianca = entrants.filter((element) => element.age < 18);
+  const adulto = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const senior = entrants.filter((element) => element.age >= 50);
+  const resultado = { adult: adulto.length, child: crianca.length, senior: senior.length };
+  return resultado;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  } if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  return countEntrants(entrants).adult * prices.adult
+   + countEntrants(entrants).child * prices.child
+    + countEntrants(entrants).senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
