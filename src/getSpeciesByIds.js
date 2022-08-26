c46c4c1604bf6266177d1e5e20f3dabc53d5f38d
petromamboubee <gm.boubee@gmail.com>
2022-08-26 15:15:40 -0300

    corrigi o lint do getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,12 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const bichos = species.filter(element => {
-    return ids.some(e => {
-      return element.id === e
-    })
-    })
-  return bichos
+  const bichos = species.filter((element) => ids.some((e) => element.id === e));
+  return bichos;
 }
-console.log(getSpeciesByIds(data))
+console.log(getSpeciesByIds(data));
 module.exports = getSpeciesByIds;
