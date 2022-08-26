bac50394145c90541d27834822f82e6d02dc87b1
petromamboubee <gm.boubee@gmail.com>
2022-08-26 05:09:29 -0300

    fiz até o test 2 da cinco e não consegui pensar condição para diferenciar o segundo if do else

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const vericaResidents = (parametro) => {
+  const solucao = species.filter((element) => element.name === parametro.specie);
+  const retorno = solucao.map((ele) => ele.residents);
+  const [indice0] = retorno;
+  return indice0;
+};
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const resultado = species.reduce((acc, crr) => {
+      acc[crr.name] = crr.residents.length;
+      return acc;
+    }, {});
+    return resultado;
+  }
+  if (typeof animal === 'object') {
+    return vericaResidents(animal).length;
+  }
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  // const [teste]  = employees;
+  // const {managers} = teste;
+  const resultado = employees.some((element) =>
+    element.managers.some((element2) =>
+      element2 === id));
+  return resultado;
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const resultado1 = employees.filter((element) =>
+    element.managers.some((element2) =>
+      element2 === managerId));
+  const solucao = resultado1.map((nome) => `${nome.firstName} ${nome.lastName}`);
+  return solucao;
 }
-
+getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
 module.exports = { isManager, getRelatedEmployees };
