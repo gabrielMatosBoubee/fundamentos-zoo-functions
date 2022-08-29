6342136e8a69f0633ef66a1e91caf5491fcf7713
petromamboubee <gm.boubee@gmail.com>
2022-08-28 21:20:26 -0300

    finalizei o requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const resultado = employees.find((element) => element.id === id);
+  const [primeiroAnimalDalista] = resultado.responsibleFor;
+  const achaAnimal = species.find((element) => element.id === primeiroAnimalDalista);
+  // const [residents] = achaAnimal.residents;
+  const AnimalMaisVelho = achaAnimal.residents.reduce((acc, crr) =>
+    (acc.age > crr.age ? acc : crr), []);
+  return Object.values(AnimalMaisVelho);
 }
 
 module.exports = getOldestFromFirstSpecies;
